6beda4ab548f7f077aabc8c15d2e4d3ad849afdc
Valentin Granados <valefcfhc@gmail.com>
2020-10-01 16:49:52 -0500

    change view

--- SnapshotTest/ContentView.swift
+++ SnapshotTest/ContentView.swift
@@ -9,8 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Color.white
+            .ignoresSafeArea()
+            .overlay(
+                Text("Hello, world!")
+                    .padding()
+            )
     }
 }
 
