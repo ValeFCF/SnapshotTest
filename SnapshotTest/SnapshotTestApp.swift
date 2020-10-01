14e2c8cecf24ce55fdf1afb9ab4a1ae757263645
Valentin Granados <valefcfhc@gmail.com>
2020-10-01 12:25:46 -0500

    add project

---
+++ SnapshotTest/SnapshotTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapshotTestApp.swift
+//  SnapshotTest
+//
+//  Created by Valentín Granados on 01/10/20.
+//
+
+import SwiftUI
+
+@main
+struct SnapshotTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
