1e3bc12c229cddf50f5b48ffdf4b3df494b05b01
Valentin Granados <valefcfhc@gmail.com>
2020-10-08 22:04:28 -0500

    .

--- SnapshotTest/ContentView.swift
+++ SnapshotTest/ContentView.swift
@@ -12,10 +12,15 @@ struct ContentView: View {
         Color.green
             .ignoresSafeArea()
             .overlay(
-                Text("Hello, world!")
-                    .padding()
+                VStack {
+                    Text("Hello, world!")
+                        .padding()
+                    label
+                }
             )
     }
+    
+    let label = Label("my device", systemImage: "iphone")
 }
 
 struct ContentView_Previews: PreviewProvider {
--- SnapshotTestTests/ContentViewTests.swift
+++ SnapshotTestTests/ContentViewTests.swift
@@ -11,9 +11,22 @@ import SwiftUI
 
 @testable import SnapshotTest
 
+private let referenceSize = CGSize(width: 200, height: 150)
+
 class ContentViewTests: XCTestCase {
     func test() {
         let contentView = UIHostingController(rootView: ContentView())
         assertSnapshot(matching: contentView, as: .image(on: .iPhoneSe))
+    }
+    
+    func testLabel() {
+        let label = ContentView().label.referenceFrame()
+        assertSnapshot(matching: label, as: .image)
+    }
+}
+
+private extension SwiftUI.View {
+    func referenceFrame() -> some View {
+        self.frame(width: referenceSize.width, height: referenceSize.height)
     }
 }
